6d0c77332b02ff0066cda10906b4f1ef4b1d2177
Aivar <avyshka@yandex.ru>
2022-07-11 18:50:42 +0200

    HW3 is done

--- Assets/Code/Launcher.cs
+++ Assets/Code/Launcher.cs
@@ -1,10 +1,24 @@
+using System;
 using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Aivagames.multiplayer
 {
-    public class Launcher : MonoBehaviourPunCallbacks
+    public class Launcher : MonoBehaviourPunCallbacks, IDisposable
     {
+        private const string ConnectText = "CONNECT";
+        private const string DisconnectText = "DISCONNECT";
+        private readonly Color ConnectColor = new Color(0.85f, 1.0f, 0.3f);
+        private readonly Color DisconnectColor = new Color(1.0f, 0.6f, 0.7f);
+
+        [SerializeField] private TMP_Text _title;
+        [SerializeField] private TMP_Text _infoLabel;
+        [SerializeField] private TMP_Text _loginButtonLabel;
+        [SerializeField] private Button _loginButton;
+
         private void Awake()
         {
             PhotonNetwork.AutomaticallySyncScene = true;
@@ -12,30 +26,81 @@ namespace Aivagames.multiplayer
 
         private void Start()
         {
-            Connect();
+            _title.text = "Photon login";
+            _loginButton.onClick.AddListener(OnConnectButtonClicked);
+        }
+
+        private void OnConnectButtonClicked()
+        {
+            _loginButton.interactable = false;
+            if (PhotonNetwork.IsConnected)
+            {
+                Disconnect();
+            }
+            else
+            {
+                Connect();
+            }
         }
 
         private void Connect()
         {
-            if (PhotonNetwork.IsConnected)
-            {
-                PhotonNetwork.JoinRandomRoom();
-            }
-            else
-            {
-                PhotonNetwork.ConnectUsingSettings();
-                PhotonNetwork.GameVersion = Application.version;
-            }
+            UpdateConnectionInfo("Connecting...", Color.white);
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = Application.version;
+        }
+
+        private void Disconnect()
+        {
+            UpdateConnectionInfo("Disconnecting...", Color.white);
+            PhotonNetwork.Disconnect();
+        }
+
+        private void JoinOrCreateRoom()
+        {
+            PhotonNetwork.JoinOrCreateRoom(
+                "RoomName",
+                new RoomOptions {MaxPlayers = 2, IsVisible = true},
+                TypedLobby.Default
+            );
+        }
+
+        private void UpdateLoginButton(string txt, Color color)
+        {
+            _loginButton.image.color = color;
+            _loginButtonLabel.text = txt;
+        }
+
+        private void UpdateConnectionInfo(string txt, Color color = default)
+        {
+            _infoLabel.color = color;
+            _infoLabel.text = txt;
+            Debug.Log(txt);
         }
 
         public override void OnConnectedToMaster()
         {
-            Debug.Log("[Photon]: OnConnectedToMaster");
+            UpdateConnectionInfo("[Photon]: OnConnectedToMaster", Color.green);
+            JoinOrCreateRoom();
         }
 
         public override void OnJoinedRoom()
         {
-            Debug.Log("[Photon]: OnJoinedRoom");
+            UpdateConnectionInfo("[Photon]: OnJoinedRoom", Color.green);
+            UpdateLoginButton(DisconnectText, DisconnectColor);
+            _loginButton.interactable = true;
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            UpdateConnectionInfo("[Photon]: OnDisconnected", Color.red);
+            UpdateLoginButton(ConnectText, ConnectColor);
+            _loginButton.interactable = true;
+        }
+
+        public void Dispose()
+        {
+            _loginButton.onClick.RemoveAllListeners();
         }
     }
 }
