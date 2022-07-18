3406ee8f926e408444c366f8361aaaa6179ea539
Aivar Sergeev <avyshka@yandex.ru>
2022-07-18 19:02:03 +0200

    Added catalog to playfab server. Added visualization for catalog items

---
+++ Assets/Code/CatalogItemInfo.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Aivagames.multiplayer
+{
+    public class CatalogItemInfo : MonoBehaviour
+    {
+        public TMP_Text ItemTitle;
+        public TMP_Text Description;
+        public TMP_Text RealMoney;
+        public TMP_Text TestCoins;
+    }
+}
