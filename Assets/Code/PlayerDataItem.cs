63d550ea0d9ce9baaf43c703a43f3e6afd69f0ef
Aivar Sergeev <avyshka@yandex.ru>
2022-07-24 13:40:53 +0200

    Added match making logic

---
+++ Assets/Code/PlayerDataItem.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Aivagames.multiplayer
+{
+    public class PlayerDataItem : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _playerNick;
+
+        public void setData(string playerNick)
+        {
+            _playerNick.text = playerNick;
+        }
+    }
+}
